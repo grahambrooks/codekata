723635bde7bbf7f034e73385c30187721ea4838a
Graham Brooks <graham@grahambrooks.com>
2014-06-07 11:46:23 -0400

    Updated javascript receipe and added a trigger call for the first build

--- recipes/js.sh
+++ recipes/js.sh
@@ -1,1 +1,31 @@
-npm install qunit
+
+function is_installed() {
+    return $(type $1 >/dev/null 2>&1)
+}
+
+function install_npm() {
+    brew install "npm"
+}
+
+function install_qunit() {
+    echo "Installing qunit"
+
+    if ! is_installed npm; then
+	install_npm
+    fi
+    
+    npm install qunit -g
+}
+
+
+function make_qunit_available() {
+    echo "JavaScript katas uses qunit"
+
+    if ! is_installed qunit; then
+	echo "Installing QUnit"
+	install_qunit
+    fi
+}
+
+
+make_qunit_available
